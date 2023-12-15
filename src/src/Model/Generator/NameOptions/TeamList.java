a5e6ce0355a7a2e7ee948be375deb881d98cfdee
ChristianHong <christian.hong.0902@gmail.com>
2023-12-14 21:44:20 -0500

    TeamList

--- src/src/Model/Generator/NameOptions/TeamList.java
+++ src/src/Model/Generator/NameOptions/TeamList.java
@@ -1,4 +1,11 @@
 package Model.Generator.NameOptions;
 
+/**
+ * Desc: Class for holding the list of all team names
+ */
 public class TeamList {
+
+    public static String [] teamNameList = {
+            "Bucks", "Celtics", "Sixers", "Warriors"};
+
 }
