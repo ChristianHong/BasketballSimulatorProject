4eb233cbc4966ce4e1ba22ba4f48ea966b0586cd
ChristianHong <christian.hong.0902@gmail.com>
2024-01-16 13:29:15 -0600

    Tests

---
+++ src/src/Tests/CoachTest.java
@@ -0,0 +1,58 @@
+package Tests;
+
+import Model.Team.Coach;
+import Model.Team.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Desc: Test class for Coach class
+ */
+class CoachTest {
+
+    private Coach coach;
+
+    @BeforeEach
+    void setUp() {
+        coach = new Coach("Phil Jackson", 65, new Team("Bulls"));
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Phil Jackson", coach.getName());
+    }
+
+    @Test
+    void setName() {
+        coach.setName("Greg Pop");
+        assertEquals("Greg Pop", coach.getName());
+    }
+
+    @Test
+    void getAge() {
+        assertEquals(65, coach.getAge());
+    }
+
+    @Test
+    void setAge() {
+        coach.increaseAge();
+        assertEquals(66, coach.getAge());
+    }
+
+    @Test
+    void getTeam() {
+        Team team = new Team("Bulls");
+        assertEquals(team, coach.getTeam());
+    }
+
+    @Test
+    void setTeam() {
+        Team team = new Team("Lakers");
+        coach.setTeam(team);
+        assertEquals(team, coach.getTeam());
+    }
+}
