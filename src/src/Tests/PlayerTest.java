3a565ca0c318409895c91b46f6edcc3e622460ce
ChristianHong <christian.hong.0902@gmail.com>
2024-01-24 13:48:16 -0600

    PlayerTest

--- src/src/Tests/PlayerTest.java
+++ src/src/Tests/PlayerTest.java
@@ -10,20 +10,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
-    
-    private Player player;
 
+    private Player player;
     @BeforeEach
     void setUp() {
         player = new Player("Lebron James", 35, Type.All_around,
                 Role.Starter, Position.Small_forward);
     }
-
     @Test
     void getType() {
         assertEquals(Type.All_around, player.getType());
     }
-
     @Test
     void increaseAge() {
         player.increaseAge();
