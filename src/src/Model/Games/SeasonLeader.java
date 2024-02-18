e6a03ad1e3c6d22e44f909aeaeb0f0cfd45c8a56
ChristianHong <christian.hong.0902@gmail.com>
2024-02-17 18:56:39 -0600

    SeasonLeader

--- src/src/Model/Games/SeasonLeader.java
+++ src/src/Model/Games/SeasonLeader.java
@@ -26,7 +26,13 @@ public class SeasonLeader {
         this.type = type;
     }
 
-    
+    /**
+     * Getter for player
+     * @return Player
+     */
+    public Player getPlayer() {
+        return player;
+    }
 
     /**
      * Getter for amount
