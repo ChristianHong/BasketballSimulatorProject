480006dba0f87d0349f7de0639f034e5c9f8049d
ChristianHong <christian.hong.0902@gmail.com>
2024-02-24 13:10:51 -0600

    Playoffs

--- src/src/Model/Games/Playoffs.java
+++ src/src/Model/Games/Playoffs.java
@@ -44,34 +44,7 @@ public class Playoffs {
         }
     }
 
-    /**
-     * Simulates a round with two teams
-     * @param team1 Team 1
-     * @param team2 Team 2
-     * @param roundType What type of round this is
-     * @return PlayoffRound object stating all round information
-     */
-    public PlayoffRound simulateRound(Team team1, Team team2, RoundType roundType){
-        int team1Wins = 0, team2Wins = 0;
-        while(team1Wins < 4 && team2Wins < 4){ //loop until a team has 4 wins
-            Game game = new Game(team1, team2);
-            game.play();
-            this.gameList.add(game);
-            if(game.getWinner().getName().equals(team1.getName())){ //team1 is winner
-                team1Wins += 1;
-            } else {
-                team2Wins += 1;
-            }
-        }
-        PlayoffRound round;
-        if(team1Wins > team2Wins){ //team1 won
-            round = new PlayoffRound(team1, team1Wins, team2, team2Wins, roundType);
-        } else{ //team2 won
-            round = new PlayoffRound(team2, team2Wins, team1, team1Wins, roundType);
-        }
-        System.out.println(round);
-        return round;
-    }
+    
 
     /**
      * Simulates the playoffs
